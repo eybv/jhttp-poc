fbaff36a11671b76303496cf655a945e4f662f19
eybv <desmont.alias@gmail.com>
2021-09-16 12:28:20 +0300

    Closed #5 - Extract Gson to field

--- src/main/java/com/github/eybv/jhttp/converter/ObjectToStringConverter.java
+++ src/main/java/com/github/eybv/jhttp/converter/ObjectToStringConverter.java
@@ -4,13 +4,15 @@ import com.google.gson.Gson;
 
 public class ObjectToStringConverter implements Converter<Object, String> {
 
+    private final Gson gson = new Gson();
+
     @Override
     public String convert(Object obj) {
         if (obj == null) return "";
         if ((obj instanceof String) || (obj instanceof Number)) {
             return String.valueOf(obj);
         }
-        return new Gson().toJson(obj);
+        return gson.toJson(obj);
     }
 
 }
